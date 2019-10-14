df002a3e331ea7b98b151b4886ac6d608bf5976b
s <git@tnu.jp>
2019-10-14 10:31:44 +0900

    Add index.ts to export

---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+export {LoginCardInfo} from "./LoginCardInfo";
+export {CardUsageStats} from "./CardUsageStats"
+
+import {VaViCrawler} from "./VaViCrawler";
+import puppeteer, {LaunchOptions} from "puppeteer";
+
+export async function launch(puppeteerLaunchOptions:LaunchOptions) {
+    const browser = await puppeteer.launch(puppeteerLaunchOptions);
+    return new VaViCrawler(browser, 'https://vvgift.jp');
+}

d3514c89da9c3d4a175da0202bd254796230b6cf
s <git@tnu.jp>
2021-07-24 15:05:15 +0900

    add comments

--- lib/VaViCrawlerBrowser.ts
+++ lib/VaViCrawlerBrowser.ts
@@ -1,10 +1,18 @@
 import puppeteer from "puppeteer";
 import { VaViCrawler } from "./VaViCrawler";
 
+/**
+ * Manages browser for crawler
+ */
 export class VaViCrawlerBrowser {
     static readonly baseUrl = 'https://vvgift.jp';
     private readonly browser: puppeteer.Browser;
 
+    /**
+     * Starts browser
+     * @param launchOptions Puppeteer launch options
+     * @returns Crawler browser instance
+     */
     static async start(launchOptions: puppeteer.LaunchOptions) : Promise<VaViCrawlerBrowser> {
         const browser = await puppeteer.launch(launchOptions)
         return new VaViCrawlerBrowser(browser);
@@ -14,6 +22,10 @@ export class VaViCrawlerBrowser {
         this.browser = browser;
     }
 
+    /**
+     * Creates new crawler
+     * @returns Crawler
+     */
     async newCrawler() : Promise<VaViCrawler> {
         const browserContext = await this.browser.createIncognitoBrowserContext();
         return new VaViCrawler(browserContext, VaViCrawlerBrowser.baseUrl)
